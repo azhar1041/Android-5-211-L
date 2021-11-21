20cf81638d0342b7d4f9368c165e27ff7743d080
azhar1041 <19630756azhar@gmail.com>
2021-11-21 15:59:37 +0700

    memanggil komponen lyout dan menampilkan nilai

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaeditText = findViewById<EditText>(R.id.namaeditText)
+        val emailditText = findViewById<EditText>(R.id.emaileditText)
+        val hpeditText = findViewById<EditText>(R.id.hpeditText)
+        val tgleditText = findViewById<EditText>(R.id.tgleditText)
+        val skseditText = findViewById<EditText>(R.id.skseditText)
+
+        simpanButton.setOnClickListener{
+            val nama = namaeditText.text.toString()
+            var sks = skseditText.text.toString().toInt()
+            sks = 160 - sks
+            Toast.makeText("sks...$sks", Toast.LENGTH_SHORT).show()
+        }
+        batalButton.setOnClickListener{
+            Toast.makeText(, "batal", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
